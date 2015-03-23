b601e056029ae25a41557641919aef48794165d5
cnb <carnav@gmail.com>
2015-03-23 19:08:21 +0100

    0.1
    as posted here:
    http://get-simple.info/forums/showthread.php?tid=5061&pid=38807#pid38807
    (2013-08-13)

---
+++ fixsiteurl.php
@@ -0,0 +1,31 @@
+<?php
+
+// register plugin
+$thisfile = basename(__FILE__, ".php");
+register_plugin(
+	$thisfile,
+	'Fix Site URL',
+	'0.1',
+	'Carlos Navarro',
+	'http://www.cyberiada.org/cnb/',
+	'Convert old to current Site URLs'
+);
+
+# add line like this, without the '#', to your gsconfig.php
+# define('OLDSITEURL','http://localhost/gstest/');
+
+add_action('edit-extras','fixsiteurl_edit'); 
+add_filter('content','fixsiteurl_content');
+
+function fixsiteurl_edit() {
+  global $content, $SITEURL;
+  if (defined('OLDSITEURL'))
+    $content = str_replace(OLDSITEURL, $SITEURL, $content);
+}
+
+function fixsiteurl_content($content) {
+  global $SITEURL;
+  if (defined('OLDSITEURL'))
+    $content = str_replace(OLDSITEURL, $SITEURL, $content);
+  return $content;
+}
